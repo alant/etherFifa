2d6310ab5347a22133d01a326faf107ae2f13f38
alant <alan.htang@gmail.com>
2018-06-07 16:12:04 +0800

    fixed title, favicon. live on ropsten

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import PropTypes from 'prop-types';
+
+ReactGA.initialize('UA-120518360-1');
+
+class GAListener extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  componentDidMount() {
+    this.sendPageView(this.context.router.history.location);
+    this.context.router.history.listen(this.sendPageView);
+  }
+
+  sendPageView(location) {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
 
 ReactDOM.render((
-<BrowserRouter>
-    <Route component={App} />
-</BrowserRouter>
+  <BrowserRouter>
+    <GAListener>
+      <Route component={App} />
+    </GAListener>
+  </BrowserRouter>
 ), document.getElementById('root')
 );
